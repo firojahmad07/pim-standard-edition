15631cffe798c3b2dbb260df2716d7449bc6e9a4
Firoj <firojahmad07@gmail.com>
2025-02-03 09:50:00 +0530

    package issue resolved

--- src/Akeneo/Platform/Bundle/CatalogVolumeMonitoringBundle/front/src/index.tsx
+++ src/Akeneo/Platform/Bundle/CatalogVolumeMonitoringBundle/front/src/index.tsx
@@ -1,23 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {ThemeProvider} from 'styled-components';
-import {pimTheme} from 'akeneo-design-system';
-import {MicroFrontendDependenciesProvider, Routes, Translations} from '@akeneo-pim-community/shared';
-import {routes} from './routes.json';
-import translations from './translations.json';
 import {CatalogVolumeMonitoringApp} from './feature';
-import {FakePIM} from './FakePIM';
-import {getMockCatalogVolume} from './feature';
+import {getCatalogVolume} from './feature/fetcher/getCatalogVolume';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={pimTheme}>
-      <MicroFrontendDependenciesProvider routes={routes as Routes} translations={translations as Translations}>
-        <FakePIM>
-          <CatalogVolumeMonitoringApp getCatalogVolumes={getMockCatalogVolume} />
-        </FakePIM>
-      </MicroFrontendDependenciesProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export {
+  CatalogVolumeMonitoringApp,
+  getCatalogVolume
+}
